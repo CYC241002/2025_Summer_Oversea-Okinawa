10d8022dea40a19ea2ca6605ec14a26295afcd08
DESKTOP-NDBTDHL\WildDagger <hoyunxian@gmail.com>
2025-05-02 16:41:42 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', function(e) {
+        const overlay = document.querySelector('.loading-overlay')
+        
+        overlay.style.opacity = 0
+        setTimeout(() => {
+            overlay.style.display = 'none'
+        }, 1000)
+
+        const slogan = document.querySelector('header.header .slogan')
+        slogan.classList.remove('start')
+    })
+})()
